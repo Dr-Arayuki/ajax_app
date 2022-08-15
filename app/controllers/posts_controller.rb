310eef4c4a8900e1247d992d519122cb8c5eaba0
Dr-Arayuki <dr.arayuki@gmail.com>
2022-08-15 11:27:46 +0700

    リクエストを処理する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,10 @@
 class PostsController < ApplicationController
-
   def index
     @posts = Post.order(id: "DESC")
   end
 
-  #def new
-  #end
-
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index 
+    post = Post.create(content: params[:content])
+    render json:{ post: post }
   end
 end
